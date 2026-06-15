a256f90d94ad96fd126f1e8b3e351b25e9f5f345
arceveoc <2500030159@kluniversity.in>
2026-06-15 16:17:23 +0530

    origins change

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routes.auth import router as auth_router
+from fastapi.middleware.cors import CORSMiddleware
 
 from database import (
     Base,
@@ -18,7 +19,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+    "https://frontend-d0rx.onrender.com"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
