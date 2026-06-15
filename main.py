52d0817250aadc6ace3b22cafe3361b452642b71
arceveoc <2500030159@kluniversity.in>
2026-06-15 16:21:06 +0530

    main change

--- main.py
+++ main.py
@@ -1,46 +1,32 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
 from routes.auth import router as auth_router
-from fastapi.middleware.cors import CORSMiddleware
-
-from database import (
-    Base,
-    engine
-)
-
 from routes import settings
 from routes import search
 
-Base.metadata.create_all(
-    bind=engine
-)
+from database import Base, engine
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-    "https://frontend-d0rx.onrender.com"
-],
-    allow_credentials=True,
+        "https://frontend-d0rx.onrender.com"
+    ],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-app.include_router(
-    settings.router
-)
-
-app.include_router(
-    search.router
-)
+app.include_router(settings.router)
+app.include_router(search.router)
 app.include_router(auth_router)
-
 
 @app.get("/")
 def home():
-
     return {
-        "message":
-        "Backend Gateway Running"
+        "message": "Backend Gateway Running"
     }
